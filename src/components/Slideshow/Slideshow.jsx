90fe07dc4abde31078b8d6c7b7ac47d6c3f6274b
Mélanie KRAEMER <m_kraemer@orange.fr>
2025-02-26 20:52:10 +0100

    Ajout backgroundimage

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -1,12 +1,30 @@
 import styles from "./Slideshow.module.scss";
+import {useState} from "react";
 
-function Slideshow({title, cover}) {
+function Slideshow({imageList}) {
+    const [imageIndex, setImageIndex] = useState(0);
+
+    const changeIndex = (newIndex) => {
+        if (newIndex < 0) {
+            newIndex = imageList.length -1;
+        } else if (newIndex > imageList.length - 1){
+            newIndex = 0;
+        }
+
+        setImageIndex(newIndex);
+    };
+
     return (
-        <div className={styles.cards} style={{ backgroundImage: `url(${cover})` }}>
-        <p className={styles.title}>{title}</p>
-  </div>
-       
-    )
-}
-
-export default Slideshow
+        <div className={styles.main} style={{ backgroundImage: `url(${imageList[imageIndex]})` }}>
+          <div className={styles.arrows}>
+            <i className={styles.iconeLeft} onClick={() => changeIndex(imageIndex - 1)}></i>
+            <i className={styles.iconeRight} onClick={() => changeIndex(imageIndex + 1)}></i>
+          </div>
+          <div className={styles.currentIndex}>
+            {imageIndex + 1} / {imageList.length}
+          </div>
+        </div>
+      );
+    }
+ 
+export default Slideshow;
--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,16 +1,32 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import styles from "./Logement.module.scss";
 import Slideshow from "../../components/Slideshow/Slideshow.jsx";
 import listelogements from "../../../data/logements.json";
-import { useLocation } from "react-router-dom";
+import Collapse from "../../components/Collapse/Collapse.jsx";
+
 
 function Logement() {
     const location = useLocation();
-  const { logement } = location.state || {};
+    const { logement } = location.state || {};
+
+    const navigate = useNavigate ();
+
+  useEffect(() => {
+    if (!logement) {
+      navigate("/error");
+    }
+  }, [logement, navigate]);
+
+  if (!logement)
+    return null;
 
     return (
       <div>
+        <div className={styles.slideshow}>
         <Slideshow imageList={logement.pictures}/>
         <p>{logement.title}</p>
+        </div>
         </div>
     )}
 
