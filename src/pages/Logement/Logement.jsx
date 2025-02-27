7d1c757d307271bfb3bbc6322494d10e4e4ecef4
Mélanie KRAEMER <m_kraemer@orange.fr>
2025-02-27 22:01:59 +0100

    Ajout info logements

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -22,12 +22,30 @@ function Logement() {
     return null;
 
     return (
-      <div>
+      <div className={styles.contenu}>
         <div className={styles.slideshow}>
         <Slideshow imageList={logement.pictures}/>
-        <p>{logement.title}</p>
+        <div className={styles.info}>
+          <div className={styles.title}>{logement.title}</div>
+          <div className={styles.location}>{logement.location}</div>
+          <div className={styles.tags}>
+            {logement.tags.map((tag, index) => (
+              <div key={index} className={styles.tag}>
+                {tag}
+              </div>
+            ))}
+          </div>
+<div className={styles.hostAndRating}>
+  <div className={styles.host}>
+    <div className={styles.hostName}>{logement.host.name}</div>
+    <img className={styles.hostPicture} src={logement.host.picture} alt="hostPicture"/>
+  </div>
+  <div className={styles.rating}>{logement.rating}</div>
+</div>
         </div>
         </div>
+        </div>
+        
     )}
 
 export default Logement
